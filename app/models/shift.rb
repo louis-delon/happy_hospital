10f07f1e9b861dfa18e20d852fb6895e5905dc64
LouisDelon <louleman34@gmail.com>
2018-11-28 21:08:43 +0100

    validate tests for shift

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,4 @@
 class Shift < ApplicationRecord
   belongs_to :worker
+  validates :start_date, presence: true
 end
--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Shift, type: :model do
   context 'Shift validation' do
     it "is valid with valid attributes" do
+      worker = create(:worker)
       shift = build(:shift)
+      worker.shifts << shift
       expect(shift).to be_valid
     end
     it { should validate_presence_of(:start_date) }
@@ -11,7 +13,10 @@ RSpec.describe Shift, type: :model do
 
   context 'Shift persistance' do
     it 'should persist a shift' do
-      create(:shift)
+      worker = create(:worker)
+      shift = build(:shift)
+      worker.shifts << shift
+      shift.save!
       expect(Shift.count).to eq(1)
     end
   end
