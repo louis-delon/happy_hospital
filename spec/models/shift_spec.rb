597843201700aeced49667f620339fdcf3f8a160
LouisDelon <louleman34@gmail.com>
2018-11-28 21:01:10 +0100

    write test for Shift model

--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Shift, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Shift validation' do
+    it "is valid with valid attributes" do
+      shift = build(:shift)
+      expect(shift).to be_valid
+    end
+    it { should validate_presence_of(:start_date) }
+  end
+
+  context 'Shift persistance' do
+    it 'should persist a shift' do
+      create(:shift)
+      expect(Shift.count).to eq(1)
+    end
+  end
+
+  context 'Shift associations' do
+    it { should belong_to(:worker) }
+  end
 end
